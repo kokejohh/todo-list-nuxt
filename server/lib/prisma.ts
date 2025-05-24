8aa97371624fcb7ecdb450cab130e4837c3ea2c7
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2025-05-25 04:02:24 +0700

    not yet

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from '@/prisma/generated/prisma-client';
-import { withAccelerate } from '@prisma/extension-accelerate';
+// import { withAccelerate } from '@prisma/extension-accelerate';
 
 // const globalForPrisma = globalThis as unknown as {
 //     prisma: ReturnType<typeof PrismaClient.prototype.$extends> | undefined 
 // }
 
 //let prisma;// = globalForPrisma.prisma ?? new PrismaClient().$extends(withAccelerate())
-const prisma = new PrismaClient().$extends(withAccelerate());
+const prisma = new PrismaClient();//.$extends(withAccelerate());
 
 // if (process.env.NODE_ENV !== 'production')
 //     globalForPrisma.prisma = prisma;
