5d3aa2601a2300c784478026feca9ee23f954ee6
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2025-05-25 04:35:44 +0700

    i think i know

--- server/api/tasks/index.get.ts
+++ server/api/tasks/index.get.ts
@@ -1,22 +1,16 @@
-// import prisma from '@/server/lib/prisma';
-
-import { PrismaClient } from '~/prisma/generated/prisma-client';
-// import { withAccelerate } from '@prisma/extension-accelerate';
+import prisma from '@/server/lib/prisma';
 
 // const globalForPrisma = globalThis as unknown as {
 //     prisma: ReturnType<typeof PrismaClient.prototype.$extends> | undefined 
 // }
 
-let prisma;// = globalForPrisma.prisma ?? new PrismaClient().$extends(withAccelerate())
-
+// let prisma;// = globalForPrisma.prisma ?? new PrismaClient().$extends(withAccelerate())
 
 export default defineEventHandler(async event => {
-    const prisma = new PrismaClient();//.$extends(withAccelerate());
-    return 1;
-    // const tasks = await prisma.tasks.findMany({
-    //     orderBy: {
-    //         order: 'asc'
-    //     }
-    // });
-    // return tasks;
+    const tasks = await prisma.tasks.findMany({
+        orderBy: {
+            order: 'asc'
+        }
+    });
+    return tasks;
 })
--- server/api/users.get.ts
+++
@@ -1,6 +0,0 @@
-import prisma from '@/server/lib/prisma';
-
-export default defineEventHandler(async (event) => {
-  const users = await prisma.user.findMany();
-  return users;
-});
