c7eb682bf78730731a61234cac737effb5c07f70
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2025-05-25 04:13:45 +0700

    keep going

--- server/api/tasks/index.get.ts
+++ server/api/tasks/index.get.ts
@@ -1,10 +1,22 @@
-import prisma from '@/server/lib/prisma';
+// import prisma from '@/server/lib/prisma';
+
+import { PrismaClient } from '@/prisma/generated/prisma-client';
+// import { withAccelerate } from '@prisma/extension-accelerate';
+
+// const globalForPrisma = globalThis as unknown as {
+//     prisma: ReturnType<typeof PrismaClient.prototype.$extends> | undefined 
+// }
+
+//let prisma;// = globalForPrisma.prisma ?? new PrismaClient().$extends(withAccelerate())
+const prisma = new PrismaClient();//.$extends(withAccelerate());
+
 
 export default defineEventHandler(async event => {
-    const tasks = await prisma.tasks.findMany({
-        orderBy: {
-            order: 'asc'
-        }
-    });
-    return tasks;
+    return 1;
+    // const tasks = await prisma.tasks.findMany({
+    //     orderBy: {
+    //         order: 'asc'
+    //     }
+    // });
+    // return tasks;
 })
