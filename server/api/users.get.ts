de8c0f33205c31f012ee2eec71df2a23f5a3d0da
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2025-05-25 02:50:39 +0700

    chore: again

--- server/api/users.get.ts
+++ server/api/users.get.ts
@@ -1,7 +1,4 @@
-import { PrismaClient } from '@/app/generated/prisma-client';
-import { withAccelerate } from '@prisma/extension-accelerate'
-
-const prisma = new PrismaClient().$extends(withAccelerate())
+import prisma from '@/server/lib/prisma';
 
 export default defineEventHandler(async (event) => {
   const users = await prisma.user.findMany();
