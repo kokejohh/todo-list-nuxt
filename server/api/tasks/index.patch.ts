e3ae31d5c8d08b6874a567bc97eaafb4186362d9
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2025-05-24 20:43:59 +0700

    feat: method patch dynamic

--- server/api/tasks/index.patch.ts
+++ server/api/tasks/index.patch.ts
@@ -3,15 +3,19 @@ import { Tasks } from '~/generated/prisma-client';
 
 
 export default defineEventHandler(async event => {
-    const body: Tasks = await readBody(event);
-    const { detail } = body;
+    const body: Partial<Tasks> = await readBody(event);
+    const { id, ...fields } = body;
+
+    if (!id) throw new Error('id is required');
+
+    const data = Object.fromEntries( Object.entries(fields).filter(([_, v]) => v !== undefined));
+
+    if (Object.keys(data).length === 0) throw new Error ('nothing to update!');
 
     const updateTask = await prisma.tasks.update({
-        data: {
-            detail,
-        },
+        data,
         where: {
-            id: body.id
+            id
         }
     });
 
